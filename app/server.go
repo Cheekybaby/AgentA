a55e78af521271ccdcbf6bb6ff0f4641fdd40eeb
Shashank Ranjan <shashankranjan07@gmail.com>
2024-10-13 19:51:33 +0530

    Feat: Serial Requests

--- app/server.go
+++ app/server.go
@@ -1,25 +1,36 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
-	"os"
+	"io"
+	"log"
 	"net"
+	"os"
+	"time"
 )
+
+var TAG_BUFFER = []byte{0x00}
 
 // Types of requests
 type RequestHeader struct {
 	RequestAPIKey	    int16    `json:"request_api_key"`
 	RequestAPIVersion	int16    `json:"request_api_version"`
 	CorrelationId		int32    `json:"correlation_id"`
-	TaggedFields		[]string `json:"tagged_fields"`
+	ClientId   			string	 `json:"client_id"`
+	TaggedFields		string   `json:"tagged_fields"`
 }
 
 func main() {
 	// Setting up a TCP listener
-	l, err := net.Listen("tcp", "0.0.0.0.9092")
+	startServer()
+}
+
+func startServer() {
+	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
-		fmt.Println("Failed to bind to the port")
+		fmt.Println("Failed to bind to the port 9092")
 		os.Exit(1)
 	}
 
@@ -32,31 +43,58 @@ func main() {
 
 	defer conn.Close() // Closes the connection after the function finishes running
 
-	// Handling the connection and taking the data
-	var buff = make([]byte, 1024)
-	conn.Read(buff)
+	for {
+		req, err := NewReqFromConn(conn)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	// Checking version
-	var version_err []byte
-	ver := binary.BigEndian.Uint16(buff[4:8])
+		res := bytes.NewBuffer([]byte{})
+		binary.Write(res, binary.BigEndian, uint32(req.CorrelationId))
+		var errorCode = uint16(0)
+		if int(req.RequestAPIVersion) < 0 || int(req.RequestAPIVersion) > 4{
+			errorCode = 35
+		}
 
-	switch ver {
-		case 0, 1, 2, 3, 4:
-			version_err = []byte{0,0}
-		default:
-			version_err = []byte{0,35}
+		binary.Write(res, binary.BigEndian, uint16(errorCode))
+		binary.Write(res, binary.BigEndian, byte(2))
+		binary.Write(res, binary.BigEndian, uint16(18))
+		binary.Write(res, binary.BigEndian, uint16(3))
+		binary.Write(res, binary.BigEndian, uint16(4))
+
+		res.Write(TAG_BUFFER)
+		binary.Write(res, binary.BigEndian, uint32(0))
+		res.Write(TAG_BUFFER)
+
+		binary.Write(conn, binary.BigEndian, uint32(res.Len()))
+
+		io.Copy(conn, res)
+	}
+}
+
+func NewReqFromConn (conn net.Conn) (RequestHeader, error) {
+	var size uint32
+	err := binary.Read(conn, binary.BigEndian, &size)
+	if err != nil {
+		return RequestHeader{}, err
 	}
 
-	// Correlation Id
-	// corr := binary.BigEndian.Uint32(buff[8:12])
-	
-	// Response
-	resp := make([]byte, 8)
-	copy(resp, []byte{0,0,0,0})
-	copy(resp[4:], buff[8:12]) // This adds the correlation id to the response
+	req := RequestHeader{}
+	binary.Read(conn, binary.BigEndian, &req.RequestAPIKey)
+	binary.Read(conn, binary.BigEndian, &req.RequestAPIVersion)
+	binary.Read(conn, binary.BigEndian, &req.CorrelationId)
 
-	resp = append(resp, version_err...)
-	
-	// The final response
-	conn.Write(resp)
+	conn.SetReadDeadline(time.Now().Add(5*time.Second))
+	conn.Read(make([]byte, 1024))
+
+	return req, nil
+}
+
+func (r *RequestHeader) Reader() (uint32, io.Reader) {
+	var buff bytes.Buffer
+	binary.Write(&buff, binary.BigEndian, &r.RequestAPIKey)
+	binary.Write(&buff, binary.BigEndian, &r.RequestAPIVersion)
+	binary.Write(&buff, binary.BigEndian, &r.CorrelationId)
+
+	return uint32(buff.Len()), &buff
 }
