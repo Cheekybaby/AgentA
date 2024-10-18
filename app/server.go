f3c103f722b3a3327f4dd194e48ffcc60e7474ca
Shashank Ranjan <shashankranjan07@gmail.com>
2024-10-18 18:23:19 +0530

    Added Description Topic Partitions Key in response

--- app/server.go
+++ app/server.go
@@ -1,104 +1,152 @@
 package main
 
 import (
-	"bytes"
 	"encoding/binary"
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"os"
-	"time"
 )
 
-var TAG_BUFFER = []byte{0x00}
+var TAG_BUFFER = byte(0)
 
 // Types of requests
 type RequestHeader struct {
 	RequestAPIKey	    int16    `json:"request_api_key"`
 	RequestAPIVersion	int16    `json:"request_api_version"`
 	CorrelationId		int32    `json:"correlation_id"`
-	ClientId   			string	 `json:"client_id"`
-	TaggedFields		string   `json:"tagged_fields"`
 }
 
 func main() {
-	// Setting up a TCP listener
+	// Starting a Server
 	startServer()
 }
 
 func startServer() {
-	l, err := net.Listen("tcp", "0.0.0.0:9092")
+	// Setting up a TCP Listener
+	listener, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to the port 9092")
 		os.Exit(1)
 	}
 
-	// Accepting incoming connections
-	for {
-		conn, err := l.Accept()
+	defer func(listener net.Listener){
+		err := listener.Close()
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
+			fmt.Println("Failed to Close Listener")
 		}
-		go handleConn(conn)
+	}(listener)
+
+	// Accepting mulitple incoming connections
+	for {
+		conn, err := listener.Accept();
+		if err != nil {
+			fmt.Println("Error in accepting connection: ", err.Error())
+			continue
+		}
+		go func() {
+			if err := handleConnection(conn);
+			err != nil {
+				fmt.Println("Error handling connections: ", err.Error())
+			}
+
+			defer func(conn net.Conn) {
+				if err := conn.Close();
+				err != nil {
+					fmt.Printf("Failed to close the connections: %s\n", err.Error())
+				}
+			} (conn)
+		}()
 	}
 }
 
-func handleConn(conn net.Conn) {
-	defer conn.Close() // Closes the connection after the function finishes running
+func handleConnection (conn net.Conn) error {
 	for {
 		req, err := NewReqFromConn(conn)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 
-		res := bytes.NewBuffer([]byte{})
-		binary.Write(res, binary.BigEndian, uint32(req.CorrelationId))
-		var errorCode = uint16(0)
-		if int(req.RequestAPIVersion) < 0 || int(req.RequestAPIVersion) > 4{
-			errorCode = 35
+		if err := handleRequest(conn, req);
+		err != nil {
+			return err
 		}
-
-		binary.Write(res, binary.BigEndian, uint16(errorCode))
-		binary.Write(res, binary.BigEndian, byte(2))
-		binary.Write(res, binary.BigEndian, uint16(18))
-		binary.Write(res, binary.BigEndian, uint16(3))
-		binary.Write(res, binary.BigEndian, uint16(4))
-
-		res.Write(TAG_BUFFER)
-		binary.Write(res, binary.BigEndian, uint32(0))
-		res.Write(TAG_BUFFER)
-
-		binary.Write(conn, binary.BigEndian, uint32(res.Len()))
-
-		io.Copy(conn, res)
 	}
 }
 
 func NewReqFromConn (conn net.Conn) (RequestHeader, error) {
+	var req RequestHeader
+	
 	var size uint32
-	err := binary.Read(conn, binary.BigEndian, &size)
-	if err != nil {
-		return RequestHeader{}, err
+	if err := binary.Read(conn, binary.BigEndian, &size);
+	err != nil {
+		return req, err
 	}
 
-	req := RequestHeader{}
-	binary.Read(conn, binary.BigEndian, &req.RequestAPIKey)
-	binary.Read(conn, binary.BigEndian, &req.RequestAPIVersion)
-	binary.Read(conn, binary.BigEndian, &req.CorrelationId)
+	buff := make([]byte, size)
+	if _, err := io.ReadFull(conn, buff);
+	err != nil {
+		return req, err
+	}
 
-	conn.SetReadDeadline(time.Now().Add(5*time.Second))
-	conn.Read(make([]byte, 1024))
+	req.RequestAPIKey = int16(binary.BigEndian.Uint16(buff[0:2]))
+	req.RequestAPIVersion = int16(binary.BigEndian.Uint16(buff[2:4]))
+	req.CorrelationId = int32(binary.BigEndian.Uint32(buff[4:8]))
 
 	return req, nil
 }
 
-func (r *RequestHeader) Reader() (uint32, io.Reader) {
-	var buff bytes.Buffer
-	binary.Write(&buff, binary.BigEndian, &r.RequestAPIKey)
-	binary.Write(&buff, binary.BigEndian, &r.RequestAPIVersion)
-	binary.Write(&buff, binary.BigEndian, &r.CorrelationId)
+func handleRequest(conn net.Conn, req RequestHeader) error {
+	var message []byte
 
-	return uint32(buff.Len()), &buff
+	// Correlation Id
+	message = binary.BigEndian.AppendUint32(message, uint32(req.CorrelationId)) // Correlation Id
+	
+	// Error Code 
+	if req.RequestAPIVersion < 0 || req.RequestAPIVersion > 4 {
+		message = binary.BigEndian.AppendUint16(message, 35) // Error in API version
+	} else {
+		message = binary.BigEndian.AppendUint16(message, 0) // No Error in API version
+
+		// Num of API Keys
+		message = append(message, byte(3))
+		// Api Versions Key
+		message = appendApiVersionsKey(message)
+		// Describe Topic Partitions Key
+		message = appendDescribeTopicPartitionsKey(message)
+		// Throttle Time
+		message = binary.BigEndian.AppendUint32(message, 0)
+		// Tag Buffer
+		message = append(message, TAG_BUFFER)
+	}
+	
+	// Message Length
+	messageLen := make([]byte, 4)
+	binary.BigEndian.PutUint32(messageLen, uint32(len(message)))
+
+	if _, err := conn.Write(messageLen);
+	err != nil {
+		fmt.Println("Error Sending Data: ", err.Error())
+	}
+	if _, err := conn.Write(message);
+	err != nil {
+		fmt.Println("Error sending data", err.Error())
+	}
+	return nil
+}
+
+func appendApiVersionsKey(message []byte) []byte {
+	message = binary.BigEndian.AppendUint16(message, 18) // Api Key
+	message = binary.BigEndian.AppendUint16(message, 0) // Min Version
+	message = binary.BigEndian.AppendUint16(message, 4) // Max Version
+	message = append(message, TAG_BUFFER) // Tag Buffer
+	return message
+}
+
+func appendDescribeTopicPartitionsKey(message []byte) []byte {
+	message = binary.BigEndian.AppendUint16(message, 75) // Topic Partitions Key
+	message = binary.BigEndian.AppendUint16(message, 0) // Min Version
+	message = binary.BigEndian.AppendUint16(message, 0) // Max Version
+	message = append(message, TAG_BUFFER) // TAG BUFFER
+	return message
 }
